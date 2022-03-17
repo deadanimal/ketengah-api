a6d4059739e692139c593ba35ac29ecaea086284
Afeez Aziz <afeezaziz@gmail.com>
2022-03-17 11:23:40 +0800

    dsa

---
+++ apps/__init__.py
@@ -0,0 +1,55 @@
+import requests
+import json
+import time
+import os
+
+from flask import Flask, render_template, flash, request, redirect, session, jsonify
+from flask_sqlalchemy import SQLAlchemy
+from decouple import config
+from flask_migrate import Migrate
+from flask_cors import CORS
+
+
+app = Flask(__name__)
+
+basedir = os.path.abspath(os.path.dirname(__file__))
+
+app.config['DEBUG'] = config('DEBUG')
+app.config['BASE_DIR'] = os.path.abspath(os.path.dirname(__file__))  
+app.config['DATABASE_CONNECT_OPTIONS'] = {}
+app.config['CSRF_SESSION_KEY'] = config('SECRET_KEY')
+app.config['SECRET_KEY'] = config('SECRET_KEY')
+app.config['CSRF_SESSION_KEY'] = config('SECRET_KEY')
+app.config['CSRF_ENABLED'] = True
+app.config['SQLALCHEMY_DATABASE_URI'] = config('DB_URL')
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+app.config['CSRF_ENABLED'] = True
+app.config['THREADS_PER_PAGE'] = 2
+
+db = SQLAlchemy(app)
+migrate = Migrate(app, db)
+CORS(app)
+
+
+@app.route('/')
+def home():
+    data_ = {
+        "name": "Ketengah API"
+    }
+    return jsonify(data_)
+
+@app.errorhandler(404)
+def not_found(error):
+    return "404", 404
+
+# from apps.admin.controllers import admin_bp as admin_module
+# app.register_blueprint(admin_module)    
+
+# from apps.blog.controllers import blog_bp as blog_module
+# app.register_blueprint(blog_module)
+
+# from apps.lotto_4d.controllers import lotto_4d_bp as lotto_4d_module
+# app.register_blueprint(lotto_4d_module)
+
+db.create_all()
+
